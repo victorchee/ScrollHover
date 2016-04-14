b5547e88b6a4cd291058c5e95dce59b1be7f60e2
Victor Chee <victor.qhj@gmail.com>
2016-04-14 11:24:52 +0800

    finish hover

--- ScrollHover/ScrollHover/ViewController.swift
+++ ScrollHover/ScrollHover/ViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var hoverView: UIView!
 
+    @IBOutlet weak var hoverTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var hoverBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var hiddenConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let topViewHeight = topView.frame.height
+        let offsetY = scrollView.contentOffset.y
+        var hoverFrame = hoverView.frame
+        
+        if offsetY >= topViewHeight {
+            hoverTopConstraint.active = false
+            hoverBottomConstraint.active = false
+            hiddenConstraint.active = true
+            
+            hoverFrame.origin.y = 0
+            hoverView.frame = hoverFrame
+            view.addSubview(hoverView)
+        } else {
+            hoverFrame.origin.y = topViewHeight
+            hoverView.frame = hoverFrame
+            scrollView.addSubview(hoverView)
+            
+            hiddenConstraint.active = false
+            hoverTopConstraint.active = true
+            hoverBottomConstraint.active = true
+        }
+    }
 }
 
